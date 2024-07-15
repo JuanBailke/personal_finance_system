4024ba397b309bbf08a3f85e0ab7d40b54064930
Juan Bailke <juan_bailke@hotmail.com>
2024-07-15 09:59:12 -0300

    Projeto criado

---
+++ src/test/java/com/zion/personal_finance_system/PersonalFinanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zion.personal_finance_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
